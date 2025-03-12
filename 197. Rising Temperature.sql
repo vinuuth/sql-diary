2eee7cc57416fa4376612d56271493d1b21f6841
Vinutha Bengaluru Prabhudev <82200148+vinuuth@users.noreply.github.com>
2025-03-12 17:41:01 -0500

    Create 197. Rising Temperature.sql
    9

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,12 @@
+SELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
+
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
+;
+
+
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
